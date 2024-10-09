ef8d55739f7f660f8892a5af67757ac73e35b60e
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2024-10-09 20:16:49 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
+// Puedes revisar mas estricto
+
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "his"];
+  let adj = ["divine", "smart", "cunning"];
+  let noun = ["book", "student", "ocean"];
+  let extension = [".com", ".org", ".es"];
+
+  let result = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          const domain = `${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}`;
+          result += domain + "<br>";
+        }
+      }
+    }
+  }
+  document.getElementById("generator").innerHTML = result;
 };
+
+/* pronoun.forEach(c =>
+  adj.forEach(d =>
+    noun.forEach(e => extension.forEach(f => console.log(`${c}${d}${e}${f}`)))
+  )
+); */
